9fbc93d2770649fb0b0fae2241719f6d7a1780a0
Kevin Cocquyt <kevin_cocquyt@outlook.com>
2023-11-29 16:59:01 +0100

    port

--- poc-components/e2e/recording-example.spec.ts
+++ poc-components/e2e/recording-example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("select options and test result", async ({ page }) => {
-    await page.goto("http://localhost:5173/");
+    await page.goto("http://localhost:5174/");
     await page.getByTestId("customer-select").selectOption("Kevin");
     await page.getByTestId("shipping-address-select").selectOption("Moerbruggeplein 4");
     await expect(page.getByTestId("form-result")).toHaveText("1 - 1");
